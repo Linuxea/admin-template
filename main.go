314538024f8849057b794b1dc6fce447f62b4160
linxiulun <linxiulun@caiji668.com>
2021-12-30 17:33:17 +0800

    ctrl

--- main.go
+++ main.go
@@ -10,24 +10,55 @@ import (
 
 func main() {
 
-	// create table person (
-	// 	id bigint(20) primary key auto_increment,
-	// 	name varchar(100) ,
-	//  pic varchar(100)
-	// )
+	yesNo := map[interface{}]interface{}{
+		"1": "是",
+		"2": "否",
+	}
+
+	colType := map[interface{}]interface{}{
+		1: "number",
+		2: "string",
+		3: "obj",
+		4: "List",
+	}
 
 	cols := []view.Column{
-
 		{
 			Prop:        "ID",
 			Label:       "id",
 			Width:       100,
 			Type:        view.Number,
 			ShowInTable: true,
-			Queryable:   false,
-			Addable:     false,
-			ShowImage:   false,
 			Color:       "blue",
+		},
+
+		{
+			Prop:        "tb_name",
+			Label:       "表名",
+			Width:       100,
+			Type:        view.String,
+			ShowInTable: true,
+			Color:       "blue",
+			Queryable:   true,
+			Addable:     true,
+		},
+		{
+			Prop:        "col_name",
+			Label:       "列名",
+			Width:       100,
+			Type:        view.String,
+			ShowInTable: true,
+			Color:       "blue",
+			Addable:     true,
+		},
+		{
+			Prop:        "label",
+			Label:       "标签名",
+			Width:       100,
+			Type:        view.String,
+			ShowInTable: true,
+			Color:       "blue",
+			Addable:     true,
 		},
 		{
 			Prop:        "type",
@@ -35,115 +66,124 @@ func main() {
 			Width:       100,
 			Type:        view.Number,
 			ShowInTable: true,
-			Queryable:   true,
+			Color:       "blue",
 			Addable:     true,
-			ShowImage:   false,
-			Required:    true,
-			Candidate: map[interface{}]interface{}{
-				1: "铭牌",
-				2: "道具",
-				3: "菜币",
-			},
+			Candidate:   colType,
 		},
 		{
-			Prop:        "type_id",
-			Label:       "类型ID",
+			Prop:        "show_in_table",
+			Label:       "列表展示",
 			Width:       100,
 			Type:        view.Number,
 			ShowInTable: true,
+			Color:       "blue",
 			Addable:     true,
-			ShowImage:   false,
+			Candidate:   yesNo,
 		},
 		{
-			Prop:        "name",
-			Label:       "名称",
-			Width:       100,
-			Type:        view.String,
-			ShowInTable: true,
-			Queryable:   true,
-			Addable:     true,
-			Required:    true,
-		},
-		{
-			Prop:        "color",
-			Label:       "颜色",
-			Width:       100,
-			Type:        view.String,
-			ShowInTable: true,
-			Queryable:   true,
-			Addable:     true,
-			Candidate: map[interface{}]interface{}{
-				"BLUE":   "蓝色",
-				"RED":    "红色",
-				"PURPLE": "紫色",
-				"YELLOW": "黄色",
-			},
-			Required:  true,
-			AutoColor: true,
-		},
-		{
-			Prop:        "unit",
-			Label:       "单位",
+			Prop:        "query_able",
+			Label:       "条件查询",
 			Width:       100,
 			Type:        view.Number,
 			ShowInTable: true,
+			Color:       "blue",
 			Addable:     true,
+			Candidate:   yesNo,
 		},
 		{
-			Prop:        "pic",
-			Label:       "图片",
+			Prop:        "add_able",
+			Label:       "支持添加/编辑",
 			Width:       100,
-			Type:        view.String,
+			Type:        view.Number,
 			ShowInTable: true,
-			Queryable:   false,
+			Color:       "blue",
 			Addable:     true,
-			ShowImage:   true,
-			Placeholder: "请上传图片",
+			Candidate:   yesNo,
+		},
+		{
+			Prop:        "show_image",
+			Label:       "图片展示",
+			Width:       100,
+			Type:        view.Number,
+			ShowInTable: true,
+			Color:       "blue",
+			Addable:     true,
+			Candidate:   yesNo,
 		},
 		{
 			Prop:        "CreatedAt",
 			Label:       "创建时间",
-			Width:       150,
+			Width:       100,
 			Type:        view.String,
 			ShowInTable: true,
-			Queryable:   false,
+			Color:       "blue",
 		},
 		{
 			Prop:        "UpdatedAt",
 			Label:       "更新时间",
-			Width:       150,
+			Width:       100,
 			Type:        view.String,
 			ShowInTable: true,
-			Queryable:   false,
+			Color:       "blue",
 		},
 	}
 
-	record := view.Record{Cols: cols, Deleteable: true}
+	record := view.Record{Cols: cols, Deleteable: true, Editable: true}
 	View := view.View{
 		Record:         record,
 		Height:         650,
 		QueryPage:      1,
 		QueryPageSize:  30,
-		ControllerName: "ActivityTwentyOneYearEndPool",
-		ModelName:      "ActivityTwnetyOneYearEndModel",
+		ControllerName: cap(sneak2Camcel("tbName")),
+		ModelName:      cap(sneak2Camcel("tbName")) + "Model",
 	}
 
-	t := template.Must(template.New("admin.txt").Funcs(template.FuncMap{
+	fm := template.FuncMap{
 		"Cap": func(in template.JS) template.JS {
-			return template.JS(strings.ToUpper(string(in[:1])) + strings.ToLower(string(in[1:])))
+			return template.JS(cap(string(in)))
 		},
-	}).ParseFiles("view/admin.txt"))
+		"Sneak2Camcel": func(in template.JS) template.JS {
+			return template.JS(sneak2Camcel(string(in)))
+		},
+	}
+
+	t := template.Must(template.New("admin.txt").Funcs(fm).ParseFiles("view/admin.txt"))
 	if err := t.Execute(os.Stdout, View); err != nil {
 		panic(err)
 	}
 
-	t = template.Must(template.New("ctrl.txt").Funcs(template.FuncMap{
-		"Cap": func(in template.JS) template.JS {
-			return template.JS(strings.ToUpper(string(in[:1])) + strings.ToLower(string(in[1:])))
-		},
-	}).ParseFiles("view/ctrl.txt"))
+	t = template.Must(template.New("ctrl.txt").Funcs(fm).ParseFiles("view/ctrl.txt"))
 	if err := t.Execute(os.Stdout, View); err != nil {
 		panic(err)
 	}
 
+}
+
+func cap(in string) string {
+	return strings.ToUpper(string(in[:1])) + strings.ToLower(string(in[1:]))
+}
+
+func sneak2Camcel(in string) string {
+	first := true
+	up := false
+	newIn := ""
+	for _, i := range in {
+
+		if first || up {
+			newIn += strings.ToUpper(string(i))
+			first = false
+			up = false
+		} else if string(i) == "_" {
+
+		} else {
+			newIn += string(i)
+		}
+
+		if string(i) == "_" {
+			up = true
+		}
+
+	}
+
+	return newIn
 }
